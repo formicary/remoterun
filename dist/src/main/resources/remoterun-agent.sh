41b18cd4ffb8781dbdc2b6ae2de6b6e7433d5209
Chris Pearson <chris@twock.com>
2015-02-26 07:23:59 +0000

    Force versions of maven-source, javadoc, and deploy plugins to help warnings on release:perform

--- dist/src/main/resources/remoterun-agent.sh
+++ dist/src/main/resources/remoterun-agent.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-ENV_NAME=$1
-JAVA=${JAVA_HOME:+${JAVA_HOME}/bin/}java
-
-$JAVA -jar lib/remoterun-agent-${project.version}.jar
+exec ${JAVA_HOME:+${JAVA_HOME}/bin/}java -jar lib/remoterun-agent-${project.version}.jar
